c572e60fe33a80fcceda139d81d1d15878e77340
Hunny <unreal5_gonza@outlook.es>
2023-05-06 00:19:25 -0300

    Agrega datos a la seccion de cursos.

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-courses',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent {
+  academies: any;
 
+  constructor(private service: DataService){}
+
+  ngOnInit(): void {
+    this.service.obtainData().subscribe((data) => {
+      this.academies = data.academies;
+    })
+  }
+
+  onAccordionClick(event: any) {
+    const $accordion = event.currentTarget;
+    $accordion.classList.toggle("is-open");
+    let content = $accordion.nextElementSibling;
+    console.log(content);
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = "8vw";
+      console.log(content.style.maxHeight);
+    }
+  }
 }
