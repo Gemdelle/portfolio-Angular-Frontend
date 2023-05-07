ce5a886300be22d95b83e82097a17a88704a59b1
Hunny <unreal5_gonza@outlook.es>
2023-05-06 21:29:59 -0300

    Agrega datos en Projects.

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-projects',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  projects: any;
 
+  constructor(private service: DataService){}
+
+  ngOnInit(): void {
+    this.service.obtainData().subscribe((data) => {
+      this.projects = data.projects.map((project: any)=> {
+        if(project.type == "HISTORY") {
+          return {
+            ...project,
+            data: {
+              ...project.data,
+              subprojects: project.data.subprojects.map((subproject:any, index: number) => {
+                return {
+                  ...subproject,
+                  isActive: index == 0
+                }
+              })
+            }
+          }
+        }
+
+        return project;
+      });
+    });
+  }
+
+  getActiveSubProject(index: number): any {
+    return this.projects[index].data.subprojects.find((subproject: any) => {
+      return subproject.isActive
+  });
+  }
 }
